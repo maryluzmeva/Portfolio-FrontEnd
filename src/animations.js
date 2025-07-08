c5e4d3021f94f23f02b616f38e4ffd17503885d9
maryluzmeva <maryluzmeva@gmail.com>
2025-07-08 12:05:09 +0200

    Add JS to the .html
    about.html, work.html and index.html adding the JS to create some scroll animations

---
+++ src/animations.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const fadeElems = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0.2,
+        rootMargin: "0px 0px -50px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('visible');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    fadeElems.forEach(elem => {
+        appearOnScroll.observe(elem);
+    });
+});
